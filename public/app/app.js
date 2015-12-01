c366b77a1b460cc4895189d932982eb0977ceb5e
Victor York <victor.a.york@gmail.com>
2015-12-01 14:22:39 -0600

    Add connections routes.

--- public/app/app.js
+++ public/app/app.js
@@ -1,28 +1,31 @@
 angular.module('dvelop', [
-  'dvelop.auth',
-  'firebase',
-  'dvelop.search',
-  'dvelop.signup',
-  'ngRoute'
-])
+    'dvelop.auth',
+    'firebase',
+    'dvelop.search',
+    'dvelop.signup',
+    'ngRoute'
+  ])
 
-.config(function($routeProvider) {
-  $routeProvider
-    .when('/auth', {
-      templateUrl: 'app/auth/auth.html',
-      controller: 'AuthController'
-    })
-    .when('/signup', {
-      templateUrl: 'app/signup/signup.html',
-      controller: 'SignupController'
-    })
-    .when('/search', {
-      templateUrl: 'app/search/search.html',
-      controller: 'SearchController as search'
-    })
-    .otherwise({
-      templateUrl: 'app/auth/auth.html',
-      controller: 'AuthController'
-    });
-
-});
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/auth', {
+        templateUrl: 'app/auth/auth.html',
+        controller: 'AuthController'
+      })
+      .when('/signup', {
+        templateUrl: 'app/signup/signup.html',
+        controller: 'SignupController'
+      })
+      .when('/search', {
+        templateUrl: 'app/search/search.html',
+        controller: 'SearchController as search'
+      })
+      .when('/connections', {
+        templateUrl: 'app/connections/connections.html',
+        controller: 'ConnectionsController'
+      })
+      .otherwise({
+        templateUrl: 'app/auth/auth.html',
+        controller: 'AuthController'
+      });
+  });
