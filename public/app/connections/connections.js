669708fe395fc13a353869cce7116ad788f1bcf7
Victor York <victor.a.york@gmail.com>
2015-12-01 19:08:01 -0600

    Save state.

--- public/app/connections/connections.js
+++ public/app/connections/connections.js
@@ -2,6 +2,9 @@ angular.module('dvelop.connections', ['dvelop.auth'])
   .controller('ConnectionsController', ['$scope', '$firebaseArray',
     function ($scope, $firebaseArray) {
       $scope.users = $firebaseArray(new Firebase("https://dvelop-carbon.firebaseio.com/users"));
+      $scope.usersConnected = [];
+      $scope.usersDisconnected = [];
+
       console.log($scope);
       console.log($scope.users);
     }
